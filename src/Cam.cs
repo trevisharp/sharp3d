ec555af0b0cae3ff5ae6aae51dfa7a695592da66
Trevisharp <leonardotrevisansilio@gmail.com>
2023-07-18 11:48:51 -0300

    Start render algorithm in camera

--- src/Cam.cs
+++ src/Cam.cs
@@ -10,20 +10,41 @@ public class Cam
 {
     private Bitmap bmp;
     private Graphics g;
+    private float d;
+    private Vertex p;
+    private Vector v;
 
-    public Vertex Location { get; set; }
-    public Vector Vector { get; set; }
+    public Vertex Location
+    {
+        get => p;
+        set
+        {
+            p = value;
+            update();
+        }
+    }
+    public Vector Vector
+    {
+        get => v;
+        set
+        {
+            v = value;
+            update();
+        }
+    }
     public float Focal { get; set; }
     public int ScreenWidth { get; init; }
     public int ScreenHeight { get; init; }
 
     public Cam(Vertex p, Vector v, float f, int wid, int hei)
     {
-        Location = p;
-        Vector = v;
+        this.p = p;
+        this.v = v;
         Focal = f;
         ScreenWidth = wid;
         ScreenHeight = hei;
+        
+        update();
 
         bmp = new Bitmap(ScreenWidth, ScreenHeight);
         g = Graphics.FromImage(bmp);
@@ -31,7 +52,22 @@ public class Cam
 
     public void Render(Scene scene)
     {
+        g.Clear(Color.Black);
+        foreach (var mesh in scene.Meshes)
+        {
+            foreach (var face in mesh.faces)
+            {
+                if (!needDraw(face.p) && !needDraw(face.q) && !needDraw(face.r))
+                    continue;
 
+                g.DrawPolygon(Pens.White, new PointF[]
+                {
+                    transform(face.p),
+                    transform(face.q),
+                    transform(face.r),
+                });
+            }
+        }
     }
 
     public void Draw(Graphics g)
@@ -40,5 +76,40 @@ public class Cam
             this.bmp,
             PointF.Empty
         );
+    }
+    
+    private void update()
+        => d = -(p.x * v.x + p.y * v.y + p.z * v.z);
+
+    private bool needDraw(Vertex q)
+        => v.x * q.x + v.y * q.y + v.z * q.z + d > 0;
+
+    private PointF transform(Vertex v)
+    {
+        // TODO: optmize
+        float a = this.v.x,
+            b = this.v.y,
+            c = this.v.z,
+            f = Focal;
+        var C = this.Location;
+        var P = v;
+
+        // ax + by + cz + d + f = 0
+        // (C - P) * t + C
+
+        // C.x + t * (C.x - P.x) = x
+        // C.y + t * (C.y - P.y) = y
+        // C.z + t * (C.z - P.z) = z
+        // a * x + b * y + c * z + d + f = 0
+
+        // a * (C.x + t * (C.x - P.x)) + b * (C.y + t * (C.y - P.y)) + c * (C.z + t * (C.z - P.z)) + d + f = 0
+        // t = -(d + f + a * C.x + b * C.y + z * C.z) / (a * (C.x - P.x) + b * (C.y - P.y) + c * (C.z - P.z))
+        float t = -(d + f + a * C.x + b * C.y + c * C.z) / (a * (C.x - P.x) + b * (C.y - P.y) + c * (C.z - P.z));
+        
+        float x = - C.x + t * (C.x - P.x);
+        float y = - C.x + t * (C.x - P.x);
+        float z = - C.x + t * (C.x - P.x);
+
+        return PointF.Empty;
     }
 }
